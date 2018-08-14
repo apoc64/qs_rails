145982f61f895f4310c49ba7ae8b0e0b51cd09a4
apoc64 <sschwedt@gmail.com>
2018-08-14 17:04:56 -0600

    creates meal model

--- spec/models/meal_spec.rb
+++ spec/models/meal_spec.rb
@@ -3,11 +3,10 @@ require 'rails_helper'
 RSpec.describe Food, type: :model do
   describe 'Validations' do
     it {should validate_presence_of :name}
-    it {should validate_presence_of :calories}
   end
 
   describe "Relationships" do
     it {should have_many :meal_foods}
-    it {should have_many(:meals).through(:meal_foods)}
+    it {should have_many(:foods).through(:meal_foods)}
   end
 end
